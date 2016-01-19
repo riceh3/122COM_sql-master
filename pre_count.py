446a750c3c10b9cd97ac3e6e97ab9dd283f6be7d
riceh3 <riceh3@uni.coventry.ac.uk>
2016-01-19 14:54:11 +0000

    Downloaded

---
+++ pre_count.py
@@ -0,0 +1,31 @@
+import sys
+import sqlite3 as sql
+
+def main():
+	sqliteFile = 'pokedex.sqlite'
+
+	try:
+		con = sql.connect(sqliteFile)
+		cur = con.cursor()
+
+		print('Which generation do you want to ask about?')
+		gen = int(input('>'))
+
+		cur.execute('''select count(*) 
+					   from pokemon_species
+					   where generation_id = ?''', (gen,))
+		count = int(cur.fetchone()[0])
+
+		print('There are %d generation %d pokemon.' % (count,gen))
+
+	except ValueError:
+		print('Generation must be an integer')
+
+	except sql.Error as e:
+		print("Error %s:" % e.args[0])
+
+	finally:
+		con.close()
+
+if __name__ == '__main__':
+	sys.exit(main())
